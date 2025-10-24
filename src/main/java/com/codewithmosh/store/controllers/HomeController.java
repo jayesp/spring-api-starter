720bb90e8327a2a24021b38c7873102ab88df8e6
Jay Espiritu <jayespiritu@yahoo.com>
2025-10-23 23:07:43 -0400

    2.6 - Passing Data to Views

--- src/main/java/com/codewithmosh/store/controllers/HomeController.java
+++ src/main/java/com/codewithmosh/store/controllers/HomeController.java
@@ -1,17 +1,14 @@
 package com.codewithmosh.store.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
-    }
-
-    @RequestMapping("/hello")
-    public String sayHello() {
-        return "index.html";
+    public String index(Model model) {
+        model.addAttribute("name", "Mosh");
+        return "index";
     }
 }
