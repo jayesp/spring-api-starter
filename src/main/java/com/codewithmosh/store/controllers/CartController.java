4cb69d0159c1d6a00af6124cfc963a60e22d80bb
Jay Espiritu <jayespiritu@yahoo.com>
2025-11-06 23:48:17 -0500

    5.6 - Getting a Cart

--- src/main/java/com/codewithmosh/store/controllers/CartController.java
+++ src/main/java/com/codewithmosh/store/controllers/CartController.java
@@ -42,7 +42,7 @@ public class CartController {
             @RequestBody AddItemToCartRequest request,
             UriComponentsBuilder uriBuilder
     ) {
-        var cart = cartRepository.findById(cartId).orElse(null);
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
         if (cart == null) {
             return ResponseEntity.notFound().build();
         }
@@ -52,7 +52,7 @@ public class CartController {
             return ResponseEntity.badRequest().build();
         }
 
-        var cartItem = cart.getCartItems()
+        var cartItem = cart.getItems()
                 .stream()
                 .filter(item -> item.getProduct().getId().equals(product.getId()))
                 .findFirst()
@@ -65,7 +65,7 @@ public class CartController {
             cartItem.setProduct(product);
             cartItem.setQuantity(1);
             cartItem.setCart(cart);
-            cart.getCartItems().add(cartItem);
+            cart.getItems().add(cartItem);
         }
 
         cartRepository.save(cart);
@@ -73,5 +73,14 @@ public class CartController {
         var cartItemDto = cartMapper.toDto(cartItem);
         var uri = uriBuilder.path("/carts/{id}/items/{itemId}").buildAndExpand(cart.getId(),cartItem.getId()).toUri();
         return ResponseEntity.created(uri).body(cartItemDto);
+    }
+
+    @GetMapping("/{cartId}")
+    public ResponseEntity<CartDto> getCart(@PathVariable UUID cartId) {
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+        if  (cart == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cartMapper.toDto(cart));
     }
 }
