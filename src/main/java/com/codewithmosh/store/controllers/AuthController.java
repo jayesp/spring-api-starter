b5b2974ab54b67199e52d6a6cb8148f448fc008a
Jay Espiritu <jayespiritu@yahoo.com>
2026-02-11 22:19:58 -0500

    6.10 - Validating JSON Web Tokens

--- src/main/java/com/codewithmosh/store/controllers/AuthController.java
+++ src/main/java/com/codewithmosh/store/controllers/AuthController.java
@@ -33,6 +33,13 @@ public class AuthController {
         return ResponseEntity.ok(new JwtResponse(token));
     }
 
+    @PostMapping("/validate")
+    public boolean validate(@RequestHeader("Authorization") String authHeader) {
+        var token = authHeader.replace("Bearer ", "");
+
+        return jwtService.validateToken(token);
+    }
+
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<Void> handleBadCredentialException() {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
