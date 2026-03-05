0388eaf2622103eb0758a8348777d707570d2545
Jay Espiritu <jayespiritu@yahoo.com>
2026-03-04 22:00:33 -0500

    7.3 - Creating Entities

---
+++ src/main/java/com/codewithmosh/store/entities/Order.java
@@ -0,0 +1,39 @@
+package com.codewithmosh.store.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "order")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private User customer;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @OneToMany(mappedBy = "order_items")
+    private Set<OrderItems> items = new HashSet<>();
+}
