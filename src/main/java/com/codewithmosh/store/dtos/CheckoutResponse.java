ae22649720727c393866d9d0fd6ab31fd3bc37c7
Jay Espiritu <jayespiritu@yahoo.com>
2026-03-15 22:32:03 -0400

    8.4 - Creating a Checkout Session

--- src/main/java/com/codewithmosh/store/dtos/CheckoutResponse.java
+++ src/main/java/com/codewithmosh/store/dtos/CheckoutResponse.java
@@ -6,8 +6,10 @@ import lombok.Data;
 @Data
 public class CheckoutResponse {
     private Long orderId;
+    private String checkoutUrl;
 
-    public CheckoutResponse(Long orderId) {
+    public CheckoutResponse(Long orderId, String checkoutUrl) {
         this.orderId = orderId;
+        this.checkoutUrl = checkoutUrl;
     }
 }
